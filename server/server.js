446c5737e7f1b3c423791f93ea84f8708971287b
SumitSingh1105 <sumitsinghchauhan1120@gmail.com>
2026-08-15 00:48:32 +0530

    Fix production CORS

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
   origin: ["http://localhost:5173",
           "http://localhost:3000",
-          "https://travellers-q3qrcyjp7-sumit-singhs-projects.vercel.app"],
+          "https://travellers-omega.vercel.app"],
   credentials: true,
 }));
 app.use(express.json());
